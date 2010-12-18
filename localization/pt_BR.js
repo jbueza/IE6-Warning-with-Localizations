9b80cbc937002adf45c69a5d2c9c987c48309452
Renato Carvalho <renatolz@gmail.com>
2010-12-18 15:11:15 +1100

    Added Brazilian Portuguese localization; Compressed the gif images with ImageOptim.app (just a few bytes less);

---
+++ localization/pt_BR.js
@@ -0,0 +1,17 @@
+//render this via backend
+window.IE6WarningLocalizations ={
+  msg1: "Você sabia que o seu Internet Explorer está desatualizado?",
+  msg2: "Para obter uma melhor experiência possível usando nosso website, recomendamos que atualize seu navegador para uma versão mais nova ou outro navegador web. Veja abaixo a lista dos navegadores web mais populares.",
+  msg3: "Basta clicar em um dos ícones para ser direcionado para a página de download",
+  br1: "Internet Explorer 7+",
+  br2: "Firefox 3+",
+  br3: "Safari 5+",
+  br4: "Opera 9.5+",
+  br5: "Chrome 5.0+",
+  url1: "http://www.microsoft.com/brasil/windows/internet-explorer/",
+  url2: "http://www.mozilla.com/pt-BR/firefox/",
+  url3: "http://www.apple.com/br/safari/download/",
+  url4: "http://www.opera.com/download/",
+  url5: "http://www.google.com/chrome?hl=pt-BR"
+};
+
